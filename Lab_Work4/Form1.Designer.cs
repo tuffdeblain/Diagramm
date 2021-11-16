19da1966d20cc7397d4d4d73360f8f8209d2899c
Sergei Kudinov <clixoxol@outlook.com>
2021-11-16 17:49:46 +0300

    Добавьте файлы проекта.

---
+++ Lab_Work4/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Diagramm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество видов товаров";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(950, 201);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(888, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(960, 223);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные о продажах";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chart1);
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(7, 278);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(962, 276);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.AccessibleName = "";
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackImageTransparentColor = System.Drawing.Color.White;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.Area3DStyle.Inclination = 50;
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.Name = "Chart1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.AutoFitMinFontSize = 8;
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 19);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "Chart1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.Transparent;
+            series1.Name = "S1";
+            series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.BrightPastel;
+            series1.ShadowColor = System.Drawing.Color.Transparent;
+            series1.YValuesPerPoint = 3;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(950, 212);
+            this.chart1.TabIndex = 3;
+            title1.BackColor = System.Drawing.Color.Transparent;
+            title1.BorderColor = System.Drawing.Color.Transparent;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.ForeColor = System.Drawing.Color.Crimson;
+            title1.Name = "Title1";
+            title1.Text = "Диаграмма продаж";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(182, 19);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(981, 595);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Построение диаграммы";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Lab_Work4/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Diagramm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = 10;
+            numericUpDown1.Minimum = 1;
+            dataGridView1.RowCount = (int)numericUpDown1.Value;
+            dataGridView1.ColumnCount = 4;
+
+            for (int i = 0; i < 4; i++)
+            {
+                dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+                dataGridView1.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+                dataGridView1.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+                dataGridView1.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+                
+                dataGridView1.Columns[0].SortMode = DataGridViewColumnSortMode.NotSortable;
+                dataGridView1.Columns[1].SortMode = DataGridViewColumnSortMode.NotSortable;
+                dataGridView1.Columns[2].SortMode = DataGridViewColumnSortMode.NotSortable;
+                dataGridView1.Columns[3].SortMode = DataGridViewColumnSortMode.NotSortable;
+
+                dataGridView1.Columns[0].HeaderCell.Value = "Название товара";
+                dataGridView1.Columns[1].HeaderCell.Value = "Цена товара";
+                dataGridView1.Columns[2].HeaderCell.Value = "Количество";
+                dataGridView1.Columns[3].HeaderCell.Value = "Стоимость";
+            }
+
+            dataGridView1.RowHeadersWidth = 50;
+
+            for (int i = 0; i < (int)numericUpDown1.Value; i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                dataGridView1.Rows[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            }
+        }
+
+
+
+
+        private void deal()
+        {
+            double count, price, priceForOne, l, sum = 0;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value == null)
+                    {
+                        dataGridView1.Rows[i].Cells[0].Value = "";
+                        dataGridView1.Rows[i].Cells[1].Value = 0;
+                        dataGridView1.Rows[i].Cells[2].Value = 0;
+                        dataGridView1.Rows[i].Cells[3].Value = 0;
+                    }
+
+                }
+            }
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+
+                dataGridView1.Columns[i].Width = 150;
+
+                if (double.TryParse(dataGridView1.Rows[i].Cells[1].Value.ToString(), out l) && double.TryParse(dataGridView1.Rows[i].Cells[2].Value.ToString(), out l) && chart1.Series["S1"].ChartType == System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie)
+                {
+
+                    string name = dataGridView1.Rows[i].Cells[0].Value.ToString();
+
+                    count = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                    price = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                    priceForOne = count * price;
+
+                    sum += priceForOne;
+                    string Percent = ((priceForOne / sum) * 100).ToString("0.00") + "%";
+
+                    dataGridView1.Rows[i].Cells[3].Value = Convert.ToString(priceForOne);
+                    chart1.Series["S1"].Label = "#PERCENT{P}";
+                    chart1.Series["S1"].LegendText = "#VALX";
+                    chart1.Series["S1"].Points.AddXY(priceForOne + " " + name, priceForOne);
+                }
+                if (double.TryParse(dataGridView1.Rows[i].Cells[1].Value.ToString(), out l) && double.TryParse(dataGridView1.Rows[i].Cells[2].Value.ToString(), out l) && chart1.Series["S1"].ChartType == System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column)
+                {
+
+                    string name = dataGridView1.Rows[i].Cells[0].Value.ToString();
+
+                    count = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                    price = Convert.ToDouble(dataGridView1.Rows[i].Cells[2].Value);
+                    priceForOne = count * price;
+
+                    sum += priceForOne;
+                    string Percent = ((priceForOne / sum) * 100).ToString("0.00") + "%";
+
+                    dataGridView1.Rows[i].Cells[3].Value = Convert.ToString(priceForOne);
+                    chart1.Series["S1"].Label = "#PERCENT{P}";
+
+
+                    chart1.Series["S1"].LegendText = "Стоимость" ;
+                    chart1.Series["S1"].Points.AddXY(priceForOne + " " + name,priceForOne);
+                }
+                if(double.TryParse(dataGridView1.Rows[i].Cells[1].Value.ToString(), out l) == false || double.TryParse(dataGridView1.Rows[i].Cells[2].Value.ToString(), out l) == false)
+                {
+                    ShowMessageBox("Введены некорректные данные", "Сообщение");
+                }
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (chart1.Series["S1"].ChartType == System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie)
+            {
+                chart1.Series.Clear();
+                chart1.Series.Add("S1");
+                deal();
+                chart1.Series["S1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+              
+            }
+            else if (chart1.Series["S1"].ChartType == System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column)
+            {
+                chart1.Series.Clear();
+                chart1.Series.Add("S1");
+                deal();
+                chart1.Series["S1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                
+            }
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (chart1.Series["S1"].ChartType == System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie)
+            {
+                chart1.Series.Clear();
+                chart1.Series.Add("S1");
+                chart1.Series["S1"].LegendText = "#VALX";
+                deal();
+                chart1.Series["S1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+             
+            }
+            else if (chart1.Series["S1"].ChartType == System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column)
+            {
+                chart1.Series.Clear();
+                chart1.Series.Add("S1");
+                deal();
+                chart1.Series["S1"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowMessageBox(string message, string text)
+        {
+            bool showed = false;
+            if (!showed)
+                MessageBox.Show(message, text);
+            showed = true;
+        }
+    }
+}
